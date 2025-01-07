c7611d7ab460b673085045fb1ef957cb1814fcd4
Reka Junardi <64375816+rekajunardi@users.noreply.github.com>
2025-01-07 17:21:11 +0700

    Create find.sh

---
+++ find.sh
@@ -0,0 +1,6 @@
+echo -e "
+Full Name : Randy Ahmad Firdaus
+Last Location : Jl Dr Ratna no 19 Jatibening Kota Bekasi
+link map : https://maps.app.goo.gl/kNPVgjCNDji1F7Nz6?g_st=ic
+Zip code : 17412
+"
